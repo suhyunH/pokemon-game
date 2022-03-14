e1d7b5a7b0dad779494d813f69f7b87cc7e31dd4
suhyunH <87814333+suhyunH@users.noreply.github.com>
2022-03-15 00:26:17 +0900

    moving backgroudn when pressing arrow button

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ class Sprite{
         this.image = image
     }
     draw(){
-        context.drawImage(this.image,-500,-340)
+        context.drawImage(this.image, this.position.x,this.position.y)
     }
 }
 const background = new Sprite({
@@ -29,6 +29,21 @@ const background = new Sprite({
     },
     image: image
 })
+
+const keys ={
+    down:{
+        pressed:false
+    },
+    up:{
+        pressed:false
+    },
+    right:{
+        pressed:false
+    },
+    left:{
+        pressed:false
+    }
+}
 
 function animate(){
     window.requestAnimationFrame(animate)
@@ -44,6 +59,15 @@ function animate(){
         playerImage.width/4,
         playerImage.height
         )
+    if(keys.down.pressed){
+        background.position.y = background.position.y-3
+    }else if(keys.up.pressed){
+        background.position.y = background.position.y+3
+    }else if(keys.right.pressed){
+        background.position.x = background.position.x-3
+    }else if(keys.left.pressed){
+        background.position.x = background.position.x+3
+    }
 }
 animate()
 
@@ -51,20 +75,45 @@ animate()
 window.addEventListener('keydown',(e)=>{
     switch(e.keyCode){
         case 40 : 
+            keys.down.pressed = true
             console.log('down') 
             break
         case 38 :
+            keys.up.pressed=true
             console.log('up') 
             break
         case 39: 
+            keys.right.pressed=true
             console.log('right')
             break
         case 37: 
+            keys.left.pressed =true
+            console.log('left')
+            break
+    }
+    console.log(keys);
+})
+
+window.addEventListener('keyup',(e)=>{
+    switch(e.keyCode){
+        case 40 : 
+            keys.down.pressed = false
+            console.log('down') 
+            break
+        case 38 :
+            keys.up.pressed= false
+            console.log('up') 
+            break
+        case 39: 
+            keys.right.pressed= false
+            console.log('right')
+            break
+        case 37: 
+            keys.left.pressed = false
             console.log('left')
             break
     }
 })
-
 
 
 
