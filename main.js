b742382b4737b0d2d006e8e27035515e8caed74b
suhyunH <87814333+suhyunH@users.noreply.github.com>
2022-03-15 00:14:23 +0900

    make a funtion for background draw

--- main.js
+++ main.js
@@ -13,20 +13,57 @@ image.src = './img/Pokemon_Game_Map.png'
 const playerImage = new Image()
 playerImage.src='./img/playerDown.png'
 
-image.onload = ()=>{
-    context.drawImage(image,-500,-340)
+class Sprite{
+    constructor({position,velocity,image}){
+        this.position = position
+        this.image = image
+    }
+    draw(){
+        context.drawImage(this.image,-500,-340)
+    }
+}
+const background = new Sprite({
+    position:{
+        x:-500,
+        y:-340
+    },
+    image: image
+})
+
+function animate(){
+    window.requestAnimationFrame(animate)
+    background.draw()
     context.drawImage(
         playerImage, 
         0,
         0,
         playerImage.width/4,
         playerImage.height,
-        canvas.width/2-playerImage.width/2+70, 
+        canvas.width/2-playerImage.width/4/2, 
         canvas.height/2-playerImage.height/2,
         playerImage.width/4,
         playerImage.height
         )
 }
+animate()
+
+
+window.addEventListener('keydown',(e)=>{
+    switch(e.keyCode){
+        case 40 : 
+            console.log('down') 
+            break
+        case 38 :
+            console.log('up') 
+            break
+        case 39: 
+            console.log('right')
+            break
+        case 37: 
+            console.log('left')
+            break
+    }
+})
 
 
 
