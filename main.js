a7592ccdc6fb3e601e463e7af4d8218c3e7184c5
suhyunH <87814333+suhyunH@users.noreply.github.com>
2022-03-14 23:00:40 +0900

    positioning map image

---
+++ main.js
@@ -0,0 +1,23 @@
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+context.fillStyle = 'white'
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image()
+image.src = './img/Pokemon_Game_Map.png'
+
+const playerImage = new Image()
+playerImage.src='./img/playerDown.png'
+
+image.onload = ()=>{
+    context.drawImage(image,-500,-340)
+    context.drawImage(playerImage, canvas.width/2-playerImage.width/2, canvas.height/2-playerImage.height/2)
+}
+
+
+
+
